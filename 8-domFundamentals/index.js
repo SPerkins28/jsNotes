a25a243d883e74628e5aee5b31de7b7e0670322d
Sean Perkins <seanperkins@Seans-MBP.hsd1.in.comcast.net>
2020-10-16 16:26:01 -0400

    10.16.20 Notes and DOM Module

---
+++ 8-domFundamentals/index.js
@@ -0,0 +1,58 @@
+let listTitle = document.getElementById('listTitle');
+console.log(listTitle);
+
+let listItem = document.querySelector('ul li');
+console.log(listItem);
+
+listTitle.style.textAlign = 'center';
+
+listItem.style.color = 'red';
+
+let grocery = document.getElementsByClassName('groceryItem');
+console.log(grocery);
+console.log(grocery[2]);
+
+let liTag = document.getElementsByTagName('li');
+console.log(liTag);
+
+let selectAll = document.querySelectorAll('ul li');
+console.log(selectAll);
+
+console.log(document.body.childNodes);
+
+let listDiv = document.getElementById('list');
+console.log(listDiv.firstChild);
+
+for(tag of liTag) { //?This is how to change every item of this tag at once
+    tag.style.fontFamily = 'cursive';
+}
+
+document.getElementsByClassName('listItem')[4].innerText = 'Buy a new cauldron';
+
+document.getElementsByClassName('groceryItem')[0].innerHTML = 'Moondew Drops';
+
+document.getElementById('clickMe').addEventListener('click', (event) => { //? Changes the button color to light blue on click
+    console.log(event);
+    console.log(event.target);
+    event.target.style.backgroundColor = 'lightBlue';
+})
+
+document.getElementById('listInput').addEventListener('keyup', (event) => {
+    console.log(event.target.value);
+})
+
+
+
+let newGrocery = document.createElement('li');
+let groceryList = document.getElementById('groceryList');
+
+groceryList.appendChild(newGrocery);
+newGrocery.innerText = "Ginger Root";
+
+let groceryTitle = document.createElement('h3');
+let groceryDiv = document.getElementById('groceryDiv');
+
+groceryDiv.appendChild(groceryTitle);
+groceryTitle.innerText = "Grocery List";
+
+groceryDiv.insertBefore(groceryTitle, groceryDiv.childNodes[0]);
